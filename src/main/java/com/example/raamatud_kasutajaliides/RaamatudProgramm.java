28d078f3c958dc2f09d081007ba24e715e26a88a
Johanna Kasenurm <johanna.kasenurm@gmail.com>
2023-04-20 13:24:57 +0300

    Peatseen

--- src/main/java/com/example/raamatud_kasutajaliides/RaamatudProgramm.java
+++ src/main/java/com/example/raamatud_kasutajaliides/RaamatudProgramm.java
@@ -1,11 +1,13 @@
 package com.example.raamatud_kasutajaliides;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -22,13 +24,15 @@ public class RaamatudProgramm extends Application {
 
          //sisselogimise ekraan
          Group SLgrupp = new Group();
-         Text sisestaNimi = new Text(50, 50, "Sisestage oma nimi:");
+         VBox SLVbox = new VBox();
+         Text sisestaNimi = new Text("Sisestage oma nimi:");
          TextField sisend = new TextField();
          Button kinnita = new Button("Logi sisse");
-         SLgrupp.getChildren().add(sisestaNimi);
-         SLgrupp.getChildren().add(sisend);
-         SLgrupp.getChildren().add(kinnita);
-         Scene algus = new Scene(SLgrupp, 500, 500);
+         SLVbox.getChildren().add(sisestaNimi);
+        SLVbox.getChildren().add(sisend);
+        SLVbox.getChildren().add(kinnita);
+        SLgrupp.getChildren().add(SLVbox);
+        Scene algus = new Scene(SLgrupp, 500, 500);
 
 
 
@@ -37,15 +41,42 @@ public class RaamatudProgramm extends Application {
         //peaekraan
         Group grupp = new Group();
 
+        /*
         GridPane ruudustik = new GridPane();
         ruudustik.setVgap(3);
         ruudustik.setHgap(3);
+         */
+
+        VBox lugemiseVBox = new VBox();
+        VBox tegevusedVBox = new VBox();
+        HBox peaHBox = new HBox();
+
+        lugemiseVBox.setSpacing(7);
+        tegevusedVBox.setSpacing(7);
+        peaHBox.setSpacing(10);
+        peaHBox.setPadding(new Insets(10, 10, 10, 10));
+
         Button loetud = new Button("Loetud raamatute nimekiri");
+        loetud.setPrefWidth(200);
         Button loen = new Button("Lugemisel raamatute nimekiri");
+        loen.setPrefWidth(200);
+
+        Button lisaRaamat1 = new Button("Lisa raamat");
+        lisaRaamat1.setPrefWidth(100);
+        Button lisaRaamat2 = new Button("Lisa raamat");
+        lisaRaamat2.setPrefWidth(100);
+
+        /*
         ruudustik.add(loen, 2, 2);
         ruudustik.add(loetud, 2, 3);
         grupp.getChildren().addAll(ruudustik);
-        Scene scene = new Scene(grupp, 500, 500);
+         */
+
+        tegevusedVBox.getChildren().addAll(lisaRaamat1, lisaRaamat2);
+        lugemiseVBox.getChildren().addAll(loetud, loen);
+        peaHBox.getChildren().addAll(lugemiseVBox, tegevusedVBox);
+        grupp.getChildren().add(peaHBox);
+        Scene scene = new Scene(grupp);
 
         //tegevused
         kinnita.setOnMouseClicked(e -> primaryStage.setScene(scene));
