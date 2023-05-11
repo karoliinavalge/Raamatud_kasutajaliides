678f4499799773fabebb34d2a7bf0ed2ce279fe6
Johanna Kasenurm <johanna.kasenurm@gmail.com>
2023-05-11 11:56:07 +0300

    Formaadierind

---
+++ src/main/java/com/example/raamatud_kasutajaliides/FormaadiErind.java
@@ -0,0 +1,8 @@
+package com.example.raamatud_kasutajaliides;
+
+public class FormaadiErind extends RuntimeException {
+    //Konstruktor
+    public FormaadiErind(String message) {
+        super(message);
+    }
+}
